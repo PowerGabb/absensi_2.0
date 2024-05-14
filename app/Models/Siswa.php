a4166f7529ef2561a45148892a0424ffb9047570
rangga bayu pratama <griyagpm@gmail.com>
2024-05-14 14:36:09 +0700

    Finish

---
+++ app/Models/Siswa.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Siswa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'siswa';
+
+	protected $fillable = ['nisn', 'nama_siswa', 'id_kelas', 'id_jurusan'];
+
+	public function kelas()
+	{
+		return $this->belongsTo(Kelas::class, 'id_kelas');
+	}
+
+	public function jurusan()
+	{
+		return $this->belongsTo(Jurusan::class, 'id_jurusan');
+	}
+}
